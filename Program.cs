5c5bdfecec3a7b9605f9c48fb170617cc3ed9de2
Francesco Nanfara <microsoft@nanfara.de>
2021-09-24 14:27:20 +0200

    Initialer Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Taschenrechner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen
+            Console.Write("Bitte gib den ersten Summanden ein: ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib den zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            // Wandel Text in Gleitkommazahlen
+            float ersterSummandAlsZahl = Convert.ToSingle(ersterSummand);
+            float zweiterSummandAlsZahl = Convert.ToSingle(zweiterSummand);
+
+            // Berechnung ausführen
+            float summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+            // Ausgabe
+            Console.WriteLine("Die Summe ist: {0}", summe);
         }
     }
 }
